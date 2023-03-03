3ccb728752cd9ce8616222c46fcb5bf598dd3c43
zhu jing yang <3161362058@qq.com>
2023-03-03 16:32:07 +0800

    feat scp communicates with the desktop
    Signed-off-by: zhu jing yang <3161362058@qq.com>

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,13 +1,48 @@
+import useSessionStore from '@/stores/session';
+import { Flex, Spinner } from '@chakra-ui/react';
 import { Nunito } from '@next/font/google';
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+import { createSealosApp, sealosApp } from 'sealos-desktop-sdk';
 import styles from './index.module.scss';
 const nunito = Nunito({ subsets: ['latin'] });
 
 export default function Layout({ children }: any) {
+  const { setSession } = useSessionStore();
+  const [isLodaing, setIsLoading] = useState(true);
+  useEffect(() => {
+    return createSealosApp({
+      appKey: 'sealos-cloud-provider'
+    });
+  }, []);
+
+  useEffect(() => {
+    const initApp = async () => {
+      try {
+        const result = await sealosApp.getUserInfo();
+        setSession(result);
+      } catch (error) {}
+      setIsLoading(false);
+    };
+    initApp();
+  }, [isLodaing, setSession]);
+
   return (
     <>
       <div className={clsx(styles.desktopContainer, nunito.className)}>
-        <main className={clsx(styles.backgroundWrap)}>{children}</main>
+        {isLodaing ? (
+          <Flex w={'100%'} h={'100%'} alignItems={'center'} justifyContent={'center'}>
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="primaryblue.600"
+              size="xl"
+            />
+          </Flex>
+        ) : (
+          <main className={clsx(styles.backgroundWrap)}>{children}</main>
+        )}
       </div>
     </>
   );
